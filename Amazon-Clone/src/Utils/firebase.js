c18834a4b6bb31b8f3cccc02ebc4225ee2f5cd6d
NJ <nej9633@gmail.com>
2025-12-17 07:44:25 +0300

    change

--- Amazon-Clone/src/Utils/firebase.js
+++ Amazon-Clone/src/Utils/firebase.js
@@ -8,12 +8,7 @@ import 'firebase/compat/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAhUnLph68Q6TSwNYNJ0rAsMB6FfaPbOng",
-  authDomain: "clone-df27d.firebaseapp.com",
-  projectId: "clone-df27d",
-  storageBucket: "clone-df27d.firebasestorage.app",
-  messagingSenderId: "824888155128",
-  appId: "1:824888155128:web:e703e073feb3eac939d40d",
+
 };
 
 // Initialize Firebase
