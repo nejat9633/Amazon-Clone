85567e0b27d7a72b49810a43c25caa66e6177543
NJ <nej9633@gmail.com>
2025-12-24 00:03:59 +0300

    render instance of firebase functions added

--- Amazon-Clone/src/Api/axiosApi.js
+++ Amazon-Clone/src/Api/axiosApi.js
@@ -2,7 +2,13 @@ import axios from "axios";
  
 const axiosInstance = axios.create(
 
-{baseURL: "http://127.0.0.1:5001/clone-df27d/us-central1/api"}
+{
+    //local instance of firebase functions
+// baseURL: "http://127.0.0.1:5001/clone-df27d/us-central1/api"
+
+//render instance of firebase functions
+    baseURL: 'https://amazon-backend-k9c4.onrender.com'
+}
 )
 
 export  {axiosInstance};
