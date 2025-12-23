e23ae354ae0917cfde1ec4e7776dd0db7ab594b8
NJ <nej9633@gmail.com>
2025-12-24 00:22:59 +0300

    render instance of firebase functions added

--- Amazon-Clone/src/Utils/firebase.js
+++ Amazon-Clone/src/Utils/firebase.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
+console.log("api KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
 
 
 // Initialize Firebase
